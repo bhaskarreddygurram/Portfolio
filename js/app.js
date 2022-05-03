f9b5ba20d2b44e2462d11fada3a0ff858f5711d1
gurram <gurram@domusware.it>
2022-05-03 22:39:26 +0200

    updates

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ const navBtn = document.querySelector("#nav-btn");
 const closeBtn = document.querySelector("#close-btn");
 const sidebar = document.querySelector("#sidebar");
 const date = document.querySelector("#date");
+const formname = document.querySelector('#formname');
+const form = document.querySelector('#myForm');
 // add fixed class to navbar
 window.addEventListener("scroll", function () {
   if (window.pageYOffset > 80) {
@@ -20,3 +22,19 @@ closeBtn.addEventListener("click", function () {
 });
 // set year
 date.innerHTML = new Date().getFullYear();
+
+function submitForm(e){
+  console.debug(' e:', e);
+}
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  console.debug( ' e:', event.target, ' this:', this);
+  //alert(this.elements.searchTerm.value)
+  let value = formname.value;
+  console.debug( ' value:', value);
+  if ( value ) {
+    window.alert("Thanks"+ value + " for contacting us. we will reach you soon");
+    form.reset();
+  }
+});
