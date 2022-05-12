48ab4c8893b389a25683a099edb419210140bf60
gurram <gurram@domusware.it>
2022-05-12 22:57:15 +0200

    Updates

--- js/app.js
+++ js/app.js
@@ -5,6 +5,19 @@ const sidebar = document.querySelector("#sidebar");
 const date = document.querySelector("#date");
 const formname = document.querySelector('#formname');
 const form = document.querySelector('#myForm');
+const list = document.querySelector('#ul');
+
+
+/*fetch("../Downloadfiles.json")
+.then(response => {
+   return response.json();
+})
+.then(jsondata => console.log(jsondata));*/
+
+const FilesData= require('../Downloadfiles.json'); 
+console.log(FilesData);
+
+console.log(FilesData.Files);
 // add fixed class to navbar
 window.addEventListener("scroll", function () {
   if (window.pageYOffset > 80) {
@@ -39,3 +52,42 @@ form.addEventListener('submit', function(event) {
   }
 });
 */
+
+let download = ['file1', 'file2']
+let output = '';
+for (i=0; i<download.length; i++) {
+  //output += '<li>' + download[i]+ '</li>';
+  output += '<li> <a href="https://www.w3schools.com/">'+download[i]+'</a></li>';
+
+}
+list.innerHTML = output;
+
+
+
+const loadJSON = (callback) => {
+  const xObj = new XMLHttpRequest();
+  xObj.overrideMimeType("application/json");
+  // 1. replace './data.json' with the local path of your file
+  xObj.open('GET', '../Downloadfiles.json', true);
+  xObj.onreadystatechange = () => {
+      if (xObj.readyState === 4 && xObj.status === 200) {
+          // 2. call your callback function
+          callback(xObj.responseText);
+      }
+  };
+  xObj.send(null);
+}
+
+const init = () => {
+  loadJSON((response) => {
+      // 3. parse JSON string into JSON Object
+      console.log('response =', response);
+      const json = JSON.parse(response);
+      console.log('your local JSON =', JSON.stringify(json, null, 4));
+      // 4. render to your page
+      //const app = document.querySelector('#app');
+      //app.innerHTML = `<pre>${JSON.stringify(json, null, 4)}</pre>`;
+  });
+}
+
+init();
